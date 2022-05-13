d4bb90d9a03377f397609b84d2074dbf36e04fe3
iostate <qmtruong92@gmail.com>
2022-05-12 22:14:36 -0700

    add scripts

--- scripts/dep.js
+++ scripts/dep.js
@@ -5,40 +5,33 @@ async function main() {
     console.warn("Use --network localhost otherwise hardhat will close the blockchain \
      immediately after deploying");
   }
-  
+
   const [owner] = await ethers.getSigners();
-  
+
   console.log("Deploying contracts with the account: ", owner.address);
-  
-  console.log("Account Balance: ", (await (await owner.getBalance()).toString()));
-  
+
+  console.log("Account Balance: ", await (await owner.getBalance()).toString());
+
   const Lottery = await ethers.getContractFactory("Lottery");
   const lottery = await Lottery.deploy();
   await lottery.deployed().then(console.log("Successfully deployed contract!"));
-  
+
   console.log("Token Address: ", lottery.address);
 }
 
 function moveABIs(lottery) {
   const fs = require("fs");
   const pathContractsDir = __dirname + "/../frontend/src/contracts";
-  
+
   if (!fs.existsSync(pathContractsDir)) {
     fs.mkDirSync(contractsDir);
   }
-  
-  fs.writeFileSync(
-    contractsDir + "/contract-address.json", 
-    JSON.stringify({Lottery: lottery.address}, undefined, 2);
-    
-  );
-  
+
+  fs.writeFileSync(contractsDir + "/contract-address.json", JSON.stringify({ Lottery: lottery.address }, undefined, 2));
+
   const LotteryArtifact = artifacts.readArtifactsSync("Lottery");
-  
-  fs.writeFileSync(
-    contractsDir + "/Lottery.json",
-    JSON.stringify(LotteryArtifact, null, 2)
-  );
+
+  fs.writeFileSync(contractsDir + "/Lottery.json", JSON.stringify(LotteryArtifact, null, 2));
 }
 
 main()
